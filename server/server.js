8ea5f367e96f2c9c1d12666001d6f417446f4209
Divyanshuk09 <123636880+Divyanshuk09@users.noreply.github.com>
2025-04-19 11:22:47 +0530

    refactor: format server.js for improved readability

--- server/server.js
+++ server/server.js
@@ -5,17 +5,22 @@ import connectDB from './DB/index.js';
 import { clerkWebhooks } from './controllers/User.controller.js';
 
 dotenv.config()
-
 const app=express()
 
+
+//middleware
 app.use(cors())
+
+//db function
 connectDB()
 
+//routes
 app.get('/',(req,res)=>{
     res.send('API is working!')
 })
 app.post('/clerk',express.json(), clerkWebhooks)
 
+//server is running on
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
